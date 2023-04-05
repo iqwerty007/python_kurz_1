e006c42424479410fc6f52389faf94a73fac3249
Barbora Simankova <simankova.b@gmail.com>
2023-04-05 15:16:06 +0200

    ukol_1

---
+++ ukol1.py
@@ -0,0 +1,15 @@
+#bonus
+jmeno_a_prijmeni = input("Napis svoje jmeno a prijmeni\n")
+
+print(jmeno_a_prijmeni.lower())
+print(jmeno_a_prijmeni.upper())
+print(jmeno_a_prijmeni[0] + "." + [6])
+
+# ukol 1
+jmeno = "Barbora"
+print((jmeno)+ "@czechitas.cz")
+# ukol 1 pomoci input
+jmeno = input("Jak se jmenujes?\n")
+print((jmeno)+ "@czechitas.cz")
+
+
